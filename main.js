04f3f381d770779ec5f2ee24f155fdad4d7ca659
arturomp99 <arturo.m.p.99@gmail.com>
2023-02-14 16:16:30 +0100

    Does not zoom over mouse cursor

--- main.js
+++ main.js
@@ -38,20 +38,22 @@ root.children.forEach(collapse);    // Collapse after the second level
 // append the svg object to the body of the page
 // appends a 'group' element to 'svg'
 // moves the 'group' element to the top left margin
-var svg = d3    
+var svg1 = d3   // Separate to avoid zoom bad behavior   
     .select("#dendrogram")
     .append("svg")
         .attr("width", width + margin.right + margin.left)
         .attr("height", height + margin.top + margin.bottom)
+let svg = svg1
     .append("g")
         .attr("transform", `translate(${margin.left},${margin.top})`);
 
 // Add zoom
-let zoom = d3.zoom().on('zoom', (d)=>{
-    console.log('ZOOM');    
-});
+// https://www.youtube.com/watch?v=ZNrG6sMNHeI
+let zoom = d3.zoom().scaleExtent([1, 10])
+    .on('zoom', (e) => svg1.attr('transform', e.transform));
 
-svg.on('zoom', zoom);
+// Add zoom.scaleExtent
+d3.select('#dendrogram').call(zoom);
 
 update(root);
 
